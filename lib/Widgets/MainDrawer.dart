ca844459accf1ecc38b8e3147bd87b8c1bb02f2b
Rahulroy-15 <83775080+Rahulroy-15@users.noreply.github.com>
2021-06-06 18:51:35 +0530

    initial commit

---
+++ lib/Widgets/MainDrawer.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:school_management/Screens/Attendance/Attendance.dart';
+import 'package:school_management/Screens/Exam/Exam_Rseult.dart';
+import 'package:school_management/Screens/Leave_Apply/Leave_apply.dart';
+import 'package:school_management/Screens/home.dart';
+import 'package:school_management/Screens/timetable.dart';
+import 'package:school_management/Widgets/DrawerListTile.dart';
+
+class MainDrawer extends StatefulWidget {
+  @override
+  _MainDrawerState createState() => _MainDrawerState();
+}
+
+class _MainDrawerState extends State<MainDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        DrawerListTile(
+            imgpath: "home.png",
+            name: "Home",
+            ontap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => Home(),
+                ),
+              );
+            }),
+        DrawerListTile(
+          imgpath: "attendance.png",
+          name: "Attendance",
+          ontap: () {
+             Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => Attendance(),
+              ),
+            );
+          },
+        ),
+         DrawerListTile(
+          imgpath: "exam.png",
+          name: "Examination",
+          ontap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => ExamResult(),
+              ),
+            );
+          },
+        ),DrawerListTile(
+            imgpath: "calendar.png", name: "Time Table", ontap: () {
+               Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => Timetable(),
+              ),
+            );
+            }),
+            DrawerListTile(
+          imgpath: "leave_apply.png",
+          name: "Leave apply",
+          ontap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => LeaveApply(),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
